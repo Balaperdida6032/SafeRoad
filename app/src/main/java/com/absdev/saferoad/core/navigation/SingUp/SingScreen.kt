efc01f1d9e59c6e1ac4952f536bb4b91158757ce
Agustín Bartaburu <162761243+Balaperdida6032@users.noreply.github.com>
2025-04-05 21:29:19 -0300

    nuevas cosas

--- app/src/main/java/com/absdev/saferoad/core/navigation/SingUp/SingScreen.kt
+++ app/src/main/java/com/absdev/saferoad/core/navigation/SingUp/SingScreen.kt
@@ -1,73 +1,130 @@
 package com.absdev.saferoad.core.navigation.SingUp
 
 import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.absdev.saferoad.ui.theme.GreenLogo
+import com.absdev.saferoad.ui.theme.ShapeButton
 import com.google.firebase.auth.FirebaseAuth
+import com.guru.fontawesomecomposelib.FaIcon
+import com.guru.fontawesomecomposelib.FaIcons
+import kotlinx.coroutines.launch
 
 @Composable
-fun SingScreen(auth:  FirebaseAuth /*navigateToHome:(String) -> Unit*/) {
+fun SingScreen(auth: FirebaseAuth /*, navigateToLogin: () -> Unit*/) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var passwordVisible by remember { mutableStateOf(false) }
-    Column(modifier = Modifier.fillMaxSize(),horizontalAlignment = Alignment.CenterHorizontally) {
-        Spacer(modifier = Modifier.weight(1f))
-        Text(text = "Register SCREEN", fontSize = 25.sp)
-        Spacer(modifier = Modifier.weight(1f))
 
-        //Campo de Usuario
-        TextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") })
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
 
-        //Campo de contrasenia
-        TextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Contraseña") },
-            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-            trailingIcon = {
-                val icon = if (passwordVisible) Icons.Default.Favorite else Icons.Default.FavoriteBorder
-                IconButton(onClick = { passwordVisible = !passwordVisible }) {
-                    Icon(imageVector = icon, contentDescription = "Toggle password visibility")
-                }
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        containerColor = Color.Black
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+
+            Text(text = "Register SCREEN", fontSize = 25.sp, color = White)
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            TextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") }
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                        if (passwordVisible) {
+                            FaIcon(faIcon = FaIcons.Eye)
+                        } else {
+                            FaIcon(faIcon = FaIcons.EyeSlash)
+                        }
+                    }
+                },
+                modifier = Modifier.padding(horizontal = 42.dp)
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Button(
+                onClick = {
+                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.i("Registrado", "Correctamente")
+                            scope.launch {
+                                snackbarHostState.showSnackbar("Usuario registrado correctamente ✅")
+                            }
+                            /*navigateToLogin()*/
+                        } else {
+                            Log.i("Registrado", "Incorrectamente")
+                            scope.launch {
+                                snackbarHostState.showSnackbar("Error al registrar el usuario ❌")
+                            }
+                        }
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+                    .padding(horizontal = 54.dp)
+                    .border(2.dp, ShapeButton, CircleShape),
+                colors = ButtonDefaults.buttonColors(containerColor = GreenLogo)
+            ) {
+                Text(text = "Confirmar")
             }
-        )
-        Spacer(modifier = Modifier.weight(1f))
 
-        Button(onClick = {
-            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-                if (task.isSuccessful){
-                    //Registrado
-                    Log.i("Registrado", "Correctamente")
-                } else {
-                    //Error
-                    Log.i("Registrado", "Inorrectamente")
-                }
-            }
-        }) {
-            Text(text = "Confirmar")
+            Spacer(modifier = Modifier.weight(1f))
         }
-        Spacer(modifier = Modifier.weight(1f))
     }
 }
+
